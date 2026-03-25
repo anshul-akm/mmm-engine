b28e9f5eefabe9d75b8478b76197565f947fe83a
Anshul Mishra <165540854+anshul-akm@users.noreply.github.com>
2026-03-26 00:06:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ footer {visibility: hidden;}
 col1, col2 = st.columns([1, 6])
 
 with col1:
-    st.image("logo.png", width=150)
+    st.image("logo2.png", width=150)
 
 with col2:
     st.markdown("<h1>MMM Intelligence</h1>", unsafe_allow_html=True)
